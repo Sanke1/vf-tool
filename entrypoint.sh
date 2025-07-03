161b660ae0829d77e9882a11b32c50a9aea6aec0
Sanke1 <sanke1337@proton.me>
2025-07-03 19:28:48 +0200

    moved config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Prüfen, ob config.json im gemounteten /app/config fehlt
 if [ ! -f /app/config/config.json ]; then
-    cp /app/config/example.config.json /app/config/config.json
+    cp /app/example.config.json /app/config/config.json
 fi
 
 exec /app/venv/bin/python main.py
