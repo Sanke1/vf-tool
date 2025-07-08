d37e556a1ab8095e9eb3a0cde928a5e8684807c0
Sanke1 <sanke1337@proton.me>
2025-07-08 23:11:22 +0200

    bugfix: Update config file handling and entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
 
-tail -f /dev/null
+cd "$(dirname "$0")"
+ls -l
+pwd
 
-# config.json aus Template erzeugen
 envsubst < config.json.template > config.json
 
-# Warten bis Datenbank erreichbar ist (optional, für Robustheit)
 until nc -z $DB_HOST $DB_PORT; do
   echo "Waiting for MariaDB..."
   sleep 2
 done
 
-# Python-App starten
 exec python main.py
